8c976eecb4113e156242867de7fcfa17bf43a9aa
pyraxo <pyraxo@pyraxo.moe>
2025-02-16 15:06:11 +0800

    Update File model
    - Add hash and created_at fields
    - Change index from name to hash

--- app/models/file.py
+++ app/models/file.py
@@ -1,11 +1,16 @@
+from datetime import datetime
+
 from beanie import Document
+from pydantic import Field
 
 
 class File(Document):
     name: str
     text: str
     tokens: int = 0
+    hash: str
+    created_at: datetime = Field(default_factory=datetime.now)
 
     class Settings:
         name = "files"
-        indexes = ["name"]
+        indexes = ["hash"]
