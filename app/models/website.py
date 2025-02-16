bc298178cdd3f2dee07ef003086a2c11fd3b252d
pyraxo <pyraxo@pyraxo.moe>
2025-02-16 15:06:32 +0800

    Update Website model

--- app/models/website.py
+++ app/models/website.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
 from beanie import Document
+from pydantic import Field
 
 
 class Website(Document):
     path: str
     content: str
+    created_at: datetime = Field(default_factory=datetime.now)
 
     class Settings:
         name = "websites"
